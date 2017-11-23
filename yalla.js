3f50d2a8b206799d16c0e61763c5a425ffda24b8
Guy Mograbi <guy.mograbi@gmail.com>
2017-11-23 12:09:04 -0800

    define tests

--- yalla.js
+++ yalla.js
@@ -1,32 +1,31 @@
 #!/usr/bin/env node
-const path = require('path');
-const YAML = require('yamljs');
-const conf = YAML.load(path.join(process.cwd(),'.yalla'))
-const _ = require('lodash');
+const path = require('path')
+const YAML = require('yamljs')
+const conf = YAML.load(path.join(process.cwd(), '.yalla'))
+const _ = require('lodash')
 
-const argv = require('minimist')(process.argv.slice(2));
-const command = _.first(argv._);
+const argv = require('minimist')(process.argv.slice(2))
+const command = _.first(argv._)
 
-
-if (!conf.hasOwnProperty(command)){
-  console.error('command [' + command + '] not in .yalla file');
-  process.exit(1);
+if (!conf.hasOwnProperty(command)) {
+  console.error('command [' + command + '] not in .yalla file')
+  process.exit(1)
 }
 
-const spawnCommand = _.template(conf[command].cmd, {interpolate: /<%=([\s\S]+?)%>/g})({argv});
+const spawnCommand = _.template(conf[command].cmd, {interpolate: /<%=([\s\S]+?)%>/g})({argv})
 
-const spawnEnv = Object.assign({}, process.env, conf[command].env);
+const spawnEnv = Object.assign({}, process.env, conf[command].env)
 
-_.each(spawnEnv, (value, key)=>{ // support objects in json
-  if (_.isObject(value)){
-    spawnEnv[key] = JSON.stringify(value);
+_.each(spawnEnv, (value, key) => { // support objects in json
+  if (_.isObject(value)) {
+    spawnEnv[key] = JSON.stringify(value)
   }
 })
 
-const { exec } = require('child_process');
-const child = exec(spawnCommand, {env: spawnEnv, stdio: 'inherit'});
-child.stdout.pipe(process.stdout);
-child.stderr.pipe(process.stderr);
+const { exec } = require('child_process')
+const child = exec(spawnCommand, {env: spawnEnv, stdio: 'inherit'})
+child.stdout.pipe(process.stdout)
+child.stderr.pipe(process.stderr)
 child.on('close', (code) => {
-  process.exit(code);
-});
+  process.exit(code)
+})
